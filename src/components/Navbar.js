9ea8aefe6bb68867954507f4c008b0cde65c11ef
TanmayDesai <tdesai@gmail.com>
2022-11-16 21:45:18 +0530

    Fixed mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     return(
     <div className="py-2  px-5 md:flex-row md:py-5 flex flex-col content-center justify-between bg-[#ffffff] navbar">
       <div className='text-4xl py-4 self-center md:text-4xl md:py-0 md:font-light'>Meme Templates</div>
-      <div className="pb-6 md:pb-0">
+      <div className="flex flex-row pb-6 md:pb-0">
       <button onClick={generateRandom} className="px-2 py-2 mx-2 text-white bg-purple-600 rounded-md hover:bg-purple-400 hover:text-black">Show another Meme</button>
       <button onClick={downloadImage} className="px-2 py-2 text-white bg-purple-600 rounded-md hover:bg-purple-400 hover:text-black">Download this Meme</button>
       </div>
