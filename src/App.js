c0e2e69b7aa6cdf70d233a2f0bfcc11076c4fbcd
TanmayDesai <tdesai@gmail.com>
2022-11-16 15:36:26 +0530

    Inital Build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { meme } from "./data/memeData";
+import './App.css'
+import { useEffect, useState } from "react";
+import {saveAs} from 'file-saver'
 
 function App() {
+
+
+  const[randomNum, setRandomNum] = useState(0)
+  const num = meme.length
+  const generateRandom = () =>{
+    setRandomNum(Math.floor(Math.random()*num))
+  }
+  
+  const downloadImage = () => {
+    saveAs(meme[randomNum].url, `${meme[randomNum].name}.jpg`)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="a bg-slate-900">
+    <div className="py-2 px-5 md:flex-row flex flex-col content-center justify-between bg-slate-200 navbar">
+      <div className='text-2xl py-4 self-center md:text-4xl md:py-0'>Meme Templates</div>
+      <div className="">
+      <button onClick={generateRandom} className="px-2 py-2 mx-2 text-white bg-purple-600 rounded-md hover:bg-purple-400 hover:text-black">Show another Meme</button>
+      <button onClick={downloadImage} className="px-2 py-2 text-white bg-purple-600 rounded-md hover:bg-purple-400 hover:text-black">Download this Meme</button>
+      </div>
     </div>
+    <div className="container">
+      <div className="card ">
+          <img className='object-scale-down' src={meme[randomNum].url} alt={meme[randomNum].name} />
+      </div>
+      </div>
+    </div>
+    
   );
 }
 
