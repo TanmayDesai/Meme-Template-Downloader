368b0be6975f85bf7e57b232d1f3fb42dca14b59
TanmayDesai <tdesai@gmail.com>
2022-11-16 16:26:47 +0530

    Made some visual changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import '../App.css'
 
 const Footer = () => {
     return(
-        <div className="py-0 bg-neutral-800 text-white px-5 md:flex-row md:py-5 flex flex-col font-mono content-center justify-center w-1/1">
+        <div className="py-0 bg-neutral-800 text-white px-5 md:flex-row md:py-5 flex flex-col font-mono content-center justify-center w-full">
         <div className='text-lg m-0'>Created by Tanmay Desai</div>
         </div>
     )
