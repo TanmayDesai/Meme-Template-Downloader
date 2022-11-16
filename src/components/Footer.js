62f83ca0913d2c46087536b0e6493129aad4d382
TanmayDesai <tdesai@gmail.com>
2022-11-16 16:30:25 +0530

    Minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../App.css'
 const Footer = () => {
     return(
         <div className="py-0 bg-neutral-800 text-white px-5 md:flex-row md:py-5 flex flex-col font-mono content-center justify-center w-full">
-        <div className='text-lg m-0'>Created by Tanmay Desai</div>
+        <div className='text-lg m-0'><a href='https://www.peerlist.io/tanmaydesai' target='_blank' >Created by Tanmay Desai</a></div>
         </div>
     )
 }
